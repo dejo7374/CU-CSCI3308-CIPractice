6a4feaf15dd7213b06bacd57ac8f28f6f10bf4b5
Devon Jones <devon.jones@colorado.edu>
2016-04-20 15:56:00 -0600

    add test case and case to sutie for area calc

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,25 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+/* coord_2d_area Test */
+START_TEST(test_2d_area)
+{
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+	
+	a.x = 0.0;
+	a.y = 0.0;
+	b.x = 1.0;
+	b.y = 0.0;
+	c.x = 1.0;
+	c.y = 1.0;
+	
+	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 0.5);
+	
+}
+END_TEST
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,11 +181,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
-
+    suite_add_tcase(s, tc_2d_area);
     /* Return Suite */
     return s;
 
